9ea57cf987d74284fccd1a0554794b9aec7adb41
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-01-12 15:08:00 +0000

    Refactor: Change components extension
    Changed all the components file extension from .js to .jsx

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import Banner from './components/Banner';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import Navbar from './components/NavBar';
-// import {NewsLetter} from './components/NewsLetter';
+import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -18,7 +17,6 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
-      {/* <NewsLetter /> */}
       <Footer />
     </>
   );
