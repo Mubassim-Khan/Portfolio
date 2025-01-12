16e0eb04ccf57de4efb87460a92bb8b6304e5166
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-01-12 15:34:27 +0000

    Refactor: Create a seperate file for links
    Put all the social links in SocialLinks.js, and mapped over it in Footer and Navbar components

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -1,106 +1,121 @@
-// import iCalculator from "../src/assets/images/iphone-calc-img.png"
-// import businessCard from "../src/assets/images/business-card.png"
-
 export const dataTab1 = [
-    {
-        id: 1,
-        title: "QuizMaster AI",
-        description: "A quiz generating AI app made using GPT-4o, Streamlit, LangChain & Python.",
-        imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/QuizMaster-AI/refs/heads/master/data/Preview.png",
-        webURL: "https://quizmaster-ai.streamlit.app/",
-        githubURL: "https://github.com/Mubassim-Khan/QuizMaster-AI",
-    },
-    {
-        id: 2,
-        title: "CarHub",
-        description: "CarHub is a Next.js based car rental app, which provides you multiple features for searching prefered car depending upon your choice.",
-        imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/CarHub/master/public/Preview.png",
-        webURL: "https://car-hub-gray-seven.vercel.app/",
-        githubURL: "https://github.com/Mubassim-Khan/CarHub",
-    },
-    {
-        id: 3,
-        title: "Spotify Clone",
-        description: "Spotify Clone is made using Next.js, Typescript, Tailwind CSS & Supabase which allows user to play different songs and much more.",
-        imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/Spotify-Clone-Next.js/master/assets/Preview.png",
-        webURL: "https://spotify-clone-ivory-sigma.vercel.app",
-        githubURL: "https://github.com/Mubassim-Khan/Spotify-Clone-Next.js",
-    },
-    {
-        id: 4,
-        title: "Imaginify AI",
-        description: "A powerful AI SaaS platform made using Next.js, Stripe, Clerk, MongoDB Atlas, Tailwind CSS which allows user to manipulate the images.",
-        imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/Imaginify-AI/refs/heads/master/public/assets/images/Preview.png",
-        webURL: "https://imaginify-ai-eight.vercel.app/",
-        githubURL: "https://github.com/Mubassim-Khan/Imaginify-AI",
-    },
-    {
-        id: 5,
-        title: "zNotebook",
-        description: "zNotebook is a cloud notebook website, made using React.js, MySQL, Node.js, Bootstrap etc.",
-        imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/zNotebook/main/src/assets/images/Preview%203.png",
-        webURL: "",
-        githubURL: "https://github.com/Mubassim-Khan/zNotebook",
-    },
-    {
-        id: 6,
-        title: "Netflix Clone",
-        description: "A Netflix-inspired Clone, built using Vanilla JavaScript to recreate the visual design and layout of the Netflix.",
-        imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/Netflix-Clone/main/Preview.jpg",
-        webURL: "https://netfflix-clone.netlify.app/",
-        githubURL: "https://github.com/Mubassim-Khan/Netflix-Clone",
-    }
-]
+  {
+    id: 1,
+    title: "QuizMaster AI",
+    description:
+      "A quiz generating AI app made using GPT-4o, Streamlit, LangChain & Python.",
+    imgURL:
+      "https://raw.githubusercontent.com/Mubassim-Khan/QuizMaster-AI/refs/heads/master/data/Preview.png",
+    webURL: "https://quizmaster-ai.streamlit.app/",
+    githubURL: "https://github.com/Mubassim-Khan/QuizMaster-AI",
+  },
+  {
+    id: 2,
+    title: "CarHub",
+    description:
+      "CarHub is a Next.js based car rental app, which provides you multiple features for searching prefered car depending upon your choice.",
+    imgURL:
+      "https://raw.githubusercontent.com/Mubassim-Khan/CarHub/master/public/Preview.png",
+    webURL: "https://car-hub-gray-seven.vercel.app/",
+    githubURL: "https://github.com/Mubassim-Khan/CarHub",
+  },
+  {
+    id: 3,
+    title: "Spotify Clone",
+    description:
+      "Spotify Clone is made using Next.js, Typescript, Tailwind CSS & Supabase which allows user to play different songs and much more.",
+    imgURL:
+      "https://raw.githubusercontent.com/Mubassim-Khan/Spotify-Clone-Next.js/master/assets/Preview.png",
+    webURL: "https://spotify-clone-ivory-sigma.vercel.app",
+    githubURL: "https://github.com/Mubassim-Khan/Spotify-Clone-Next.js",
+  },
+  {
+    id: 4,
+    title: "Imaginify AI",
+    description:
+      "A powerful AI SaaS platform made using Next.js, Stripe, Clerk, MongoDB Atlas, Tailwind CSS which allows user to manipulate the images.",
+    imgURL:
+      "https://raw.githubusercontent.com/Mubassim-Khan/Imaginify-AI/refs/heads/master/public/assets/images/Preview.png",
+    webURL: "https://imaginify-ai-eight.vercel.app/",
+    githubURL: "https://github.com/Mubassim-Khan/Imaginify-AI",
+  },
+  {
+    id: 5,
+    title: "zNotebook",
+    description:
+      "zNotebook is a cloud notebook website, made using React.js, MySQL, Node.js, Bootstrap etc.",
+    imgURL:
+      "https://raw.githubusercontent.com/Mubassim-Khan/zNotebook/main/src/assets/images/Preview%203.png",
+    webURL: "",
+    githubURL: "https://github.com/Mubassim-Khan/zNotebook",
+  },
+  {
+    id: 6,
+    title: "Netflix Clone",
+    description:
+      "A Netflix-inspired Clone, built using Vanilla JavaScript to recreate the visual design and layout of the Netflix.",
+    imgURL:
+      "https://raw.githubusercontent.com/Mubassim-Khan/Netflix-Clone/main/Preview.jpg",
+    webURL: "https://netfflix-clone.netlify.app/",
+    githubURL: "https://github.com/Mubassim-Khan/Netflix-Clone",
+  },
+];
 
 export const dataTab2 = [
-    {
-        id: 1,
-        title: "Travel Journal",
-        description: "A Travel Journal is a dynamic website, having cards of different of places along with its image, description, link of Google Maps & much more.",
-        imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/My-Travel-Journal/main/src/Components/Preview_Home.png",
-        webURL: "https://m-travel-journal.netlify.app/",
-        githubURL: "https://github.com/Mubassim-Khan/My-Travel-Journal",
-    },
-    {
-        id: 2,
-        title: "DotCom Communications",
-        description: "A dynamic website which creates the visual UI/UX design and layout of landing & login page.",
-        imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/Landing---Login-Page/main/src/Assets/Preview.png",
-        webURL: "https://m-dotcom.netlify.app/",
-        githubURL: "https://github.com/Mubassim-Khan/Landing---Login-Page",
-    },
-    // {
-    //     id: 3,
-    //     title: "MAK News App",
-    //     description: "A fast, efficient & updated news app, having multiple options & categories of news. Get updated to the world's news of your favorite choice & type.",
-    //     imgURL: "",
-    //     webURL: "",
-    //     githubURL: "https://github.com/Mubassim-Khan/News-App-ReactJS",
-    // },
-    // {
-    //     id: 4,
-    //     title: "Text Tools",
-    //     description: "Text Tools is React.js based app, which provides you various features to manipulate your text in the way you want.",
-    //     imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/TextTools-React-JS/master/src/Assets/Preview.png",
-    //     webURL: "https://mtexttools.netlify.app/",
-    //     githubURL: "https://github.com/Mubassim-Khan/TextTools-React-JS",
-    // },
-    {
-        id: 5,
-        title: "Meme Generator",
-        description: "Meme Generator allows user to create a meme by inserting text & generators random meme images.",
-        imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/Meme-Generator/main/src/Assets/Capture.png",
-        webURL: "https://m-meme-generator.netlify.app/",
-        githubURL: "https://github.com/Mubassim-Khan/Meme-Generator",
-    },
-    // {
-    //     id: 6,
-    //     title: "Ticket Booking Assistant",
-    //     description: "An AI which books ticket for to save your time. This is made possible by using dialog flow chatbot.",
-    //     imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/Ticket-Booking-Assistant/master/assets/Preview.png",
-    //     webURL: "https://ticket-booking-assistant.netlify.app/",
-    //     githubURL: "https://github.com/Mubassim-Khan/MLSA-BootCamp-Project-Ticket-Booking-Assistant-",
-    // }
-]
+  {
+    id: 1,
+    title: "Travel Journal",
+    description:
+      "A Travel Journal is a dynamic website, having cards of different of places along with its image, description, link of Google Maps & much more.",
+    imgURL:
+      "https://raw.githubusercontent.com/Mubassim-Khan/My-Travel-Journal/main/src/Components/Preview_Home.png",
+    webURL: "https://m-travel-journal.netlify.app/",
+    githubURL: "https://github.com/Mubassim-Khan/My-Travel-Journal",
+  },
+  {
+    id: 2,
+    title: "DotCom Communications",
+    description:
+      "A dynamic website which creates the visual UI/UX design and layout of landing & login page.",
+    imgURL:
+      "https://raw.githubusercontent.com/Mubassim-Khan/Landing---Login-Page/main/src/Assets/Preview.png",
+    webURL: "https://m-dotcom.netlify.app/",
+    githubURL: "https://github.com/Mubassim-Khan/Landing---Login-Page",
+  },
+  // {
+  //     id: 3,
+  //     title: "MAK News App",
+  //     description: "A fast, efficient & updated news app, having multiple options & categories of news. Get updated to the world's news of your favorite choice & type.",
+  //     imgURL: "",
+  //     webURL: "",
+  //     githubURL: "https://github.com/Mubassim-Khan/News-App-ReactJS",
+  // },
+  // {
+  //     id: 4,
+  //     title: "Text Tools",
+  //     description: "Text Tools is React.js based app, which provides you various features to manipulate your text in the way you want.",
+  //     imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/TextTools-React-JS/master/src/Assets/Preview.png",
+  //     webURL: "https://mtexttools.netlify.app/",
+  //     githubURL: "https://github.com/Mubassim-Khan/TextTools-React-JS",
+  // },
+  {
+    id: 5,
+    title: "Meme Generator",
+    description:
+      "Meme Generator allows user to create a meme by inserting text & generators random meme images.",
+    imgURL:
+      "https://raw.githubusercontent.com/Mubassim-Khan/Meme-Generator/main/src/Assets/Capture.png",
+    webURL: "https://m-meme-generator.netlify.app/",
+    githubURL: "https://github.com/Mubassim-Khan/Meme-Generator",
+  },
+  // {
+  //     id: 6,
+  //     title: "Ticket Booking Assistant",
+  //     description: "An AI which books ticket for to save your time. This is made possible by using dialog flow chatbot.",
+  //     imgURL: "https://raw.githubusercontent.com/Mubassim-Khan/Ticket-Booking-Assistant/master/assets/Preview.png",
+  //     webURL: "https://ticket-booking-assistant.netlify.app/",
+  //     githubURL: "https://github.com/Mubassim-Khan/MLSA-BootCamp-Project-Ticket-Booking-Assistant-",
+  // }
+];
 
-export const dataTab3 = []
+export const dataTab3 = [];
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from 'react';
-import { Container, Navbar, Nav } from 'react-bootstrap';
-// import Logo from '../assets/images/logo.png';
-import navIcon1 from '../assets/images/nav-icon1.svg'
-// import navIcon3 from '../assets/images/nav-icon3.svg'
-import githubIcon from "../assets/images/githubIcon.svg"
-import { BrowserRouter, Link } from "react-router-dom"
-import { HashLink } from "react-router-hash-link"
+import { useEffect, useState } from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { BrowserRouter, Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+
+import { links } from "../SocialLinks";
 
 export default function NavBar() {
   const [scrolled, setScrolled] = useState(false);
@@ -13,24 +11,24 @@ export default function NavBar() {
   useEffect(() => {
     const onScroll = () => {
       if (window.scrollY > 50) {
-        setScrolled(true)
+        setScrolled(true);
       } else {
-        setScrolled(false)
+        setScrolled(false);
       }
-    }
+    };
 
     window.addEventListener("scroll", onScroll);
 
     return () => window.removeEventListener("scroll", onScroll);
-  }, [])
+  }, []);
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
     <BrowserRouter>
-      <Navbar expand="md" className={scrolled ? "scrolled" : ''}>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           {/* <Navbar.Brand to="#home">
           <img className="navbar-logo" src={Logo} alt="Logo" />
@@ -40,21 +38,50 @@ export default function NavBar() {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icons">
-                <Link to="https://www.linkedin.com/in/mubassim" target="_blank" rel="noopener noreferrer">
-                  <img src={navIcon1} alt="LinkedIn" />
-                </Link>
-                <Link to="https://www.github.com/Mubassim-Khan" target="_blank" rel="noopener noreferrer">
-                  <img src={githubIcon} alt="GitHub" />
-                </Link>
-                {/* <Link to="https://www.instagram.com/mubassimkhan_08/?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D">
-                  <img src={navIcon3} alt="Instagram" />
-                </Link> */}
+                {links.slice(0, 2).map((link, id) => {
+                  return (
+                    <Link
+                      key={id}
+                      to={link.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img src={link.imgSrc} alt={link.alt} />
+                    </Link>
+                  );
+                })}
               </div>
               <span>
                 <HashLink to="#connect">
